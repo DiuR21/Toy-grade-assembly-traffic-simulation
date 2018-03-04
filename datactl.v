83d062e33bb54e0abf81b47ae983a3b7d351b66c
Hekuenra <hekuenra@HekuenradeMacBook-Pro.local>
2018-03-04 13:35:46 +0800

    TGATS

--- datactl.v
+++
@@ -1,7 +0,0 @@
-module datactl(
-                input data_ena,
-                input [7:0] in,
-                output [7:0] data);
-  
-  assign data = (data_ena) ? in : 8'bzzzz_zzzz;
-endmodule
